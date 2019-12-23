91b3c0ed1ccf1acddcf76230a7e49c937b7a3aae
Mhd Omar Bahra <omaralbahra@gmail.com>
2019-12-23 20:35:14 +0300

    #3 login ui is done and intents for both pages login and register

--- app/src/main/java/com/example/cryptomoney/RegisterActivity.java
+++ app/src/main/java/com/example/cryptomoney/RegisterActivity.java
@@ -1,13 +1,30 @@
 package com.example.cryptomoney;
-
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
-
+  Button login_button;
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    try {
+      this.getSupportActionBar().hide();
+    } catch (NullPointerException e) {
+    }
     setContentView(R.layout.activity_register);
+
+    login_button = findViewById(R.id.login_button);
+
+    login_button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+      }
+    });
   }
 }
